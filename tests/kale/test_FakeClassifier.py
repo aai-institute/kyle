5c260c8de674898073e091527deee4519795f292
Michael Panchenko <michael.panchenko@unternehmertum.de>
2020-06-14 19:13:45 +0200

    sampling: enhanced test

--- tests/kale/test_FakeClassifier.py
+++ tests/kale/test_FakeClassifier.py
@@ -2,7 +2,11 @@ from kale.sampling import FakeClassifier
 
 
 def test_FakeClassifier():
-    faker = FakeClassifier(3)
+    n_classes, n_samples = 3, 5
+    faker = FakeClassifier(n_classes)
     ground_truth, class_proba = faker.get_sample()
-    assert ground_truth in [0, 1, 2]
-    assert len(class_proba) == 3
+    ground_truth_array, class_proba_array = faker.get_sample_arrays(5)
+    assert ground_truth in range(n_classes)
+    assert class_proba.shape == (n_classes,)
+    assert ground_truth_array.shape == (n_samples, )
+    assert class_proba_array.shape == (n_samples, n_classes)
