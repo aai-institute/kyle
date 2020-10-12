c696d04056442fb16a75c7ff82c7b2764ea36922
Abdul-Moeed <abdulmoeed444@gmail.com>
2020-10-12 16:27:46 +0200

    Add input validation to MCE and ACE.

--- src/metrics/ace.py
+++ src/metrics/ace.py
@@ -1,8 +1,19 @@
+from typing import Union
+import numpy as np
+
 from netcal.metrics import ACE as netcal_ACE
 
 from metrics.base_calibration_error import BaseCalibrationError
 
 
-class ACE(BaseCalibrationError, netcal_ACE):
-    def __init__(self):
-        super().__init__()
+class ACE(BaseCalibrationError):
+    def __init__(self, bins: int = 10):
+        super(ACE, self).__init__()
+        self.netcal_ace = netcal_ACE(bins=bins)
+
+    def measure(self, confidences: np.ndarray, ground_truth: np.ndarray, **kwargs)\
+            -> Union[float, np.ndarray, ValueError]:
+        input_is_invalid, error_message = self.check_input_is_invalid(confidences, ground_truth)
+        if input_is_invalid:
+            raise ValueError(error_message)
+        return self.netcal_ace.measure(confidences, ground_truth, **kwargs)
--- src/metrics/mce.py
+++ src/metrics/mce.py
@@ -1,8 +1,19 @@
+from typing import Union
+import numpy as np
+
 from netcal.metrics import MCE as netcal_MCE
 
 from metrics.base_calibration_error import BaseCalibrationError
 
 
-class MCE(BaseCalibrationError, netcal_MCE):
-    def __init__(self):
-        super().__init__()
+class MCE(BaseCalibrationError):
+    def __init__(self, bins: int = 10):
+        super(MCE, self).__init__()
+        self.netcal_mce = netcal_MCE(bins=bins)
+
+    def measure(self, confidences: np.ndarray, ground_truth: np.ndarray, **kwargs)\
+            -> Union[float, np.ndarray, ValueError]:
+        input_is_invalid, error_message = self.check_input_is_invalid(confidences, ground_truth)
+        if input_is_invalid:
+            raise ValueError(error_message)
+        return self.netcal_mce.measure(confidences, ground_truth, **kwargs)
