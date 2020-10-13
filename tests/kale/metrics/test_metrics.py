004e4d6458fcb55f112429835e3e9f63955ce641
Abdul-Moeed <abdulmoeed444@gmail.com>
2020-10-13 15:12:10 +0200

    Move metric tests inside tests/kale directory. Move function in utils.py to test_metrics.py Delete utils.py

--- tests/kale/metrics/test_metrics.py
+++ tests/kale/metrics/test_metrics.py
@@ -1,7 +1,8 @@
 import pytest
+import numpy as np
 
-from metrics import ECE, MCE, ACE
-from tests.metrics.utils import sample_confs_from_dirichlet
+from kale.metrics import ECE, MCE, ACE
+from kale.sampling.fake_clf import DirichletFC
 
 
 @pytest.fixture()
@@ -10,7 +11,12 @@ def criteria():
     return criteria
 
 
-def test_string_input_validation(criteria):
+def sample_confs_from_dirichlet(n_classes: int = 2, n_samples: int = 1000) -> np.ndarray:
+    faker = DirichletFC(n_classes)
+    return faker.get_sample_arrays(n_samples)
+
+
+def test_string_input_validation(criteria: list):
     for criterion in criteria:
         with pytest.raises(ValueError):
             assert criterion.measure(["8"], ["10"])
@@ -18,7 +24,7 @@ def test_string_input_validation(criteria):
             assert criterion.measure(["8", [1, 2]])
 
 
-def test_confidences_sum_to_one(criteria):
+def test_confidences_sum_to_one(criteria: list):
     for criterion in criteria:
         ground_truth, confidences = sample_confs_from_dirichlet(n_classes=2, n_samples=1000)
         confidences += 0.5
@@ -26,7 +32,7 @@ def test_confidences_sum_to_one(criteria):
             assert criterion.measure(confidences, ground_truth)
 
 
-def test_with_calibrated_confidences(criteria):
+def test_with_calibrated_confidences(criteria: list):
     for criterion in criteria:
         ground_truth, confidences = sample_confs_from_dirichlet(n_classes=2, n_samples=1000)
         epsilon = 0.1
