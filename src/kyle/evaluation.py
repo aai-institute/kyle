cd9084024ca675df5f4f7ae20c2819680f75e0d1
Michael Panchenko <michael.panchenko@unternehmertum.de>
2020-11-11 23:51:27 +0100

    Significantly extended the evaluation module, removed netcal dependency
    We have new metrics, new quantities, improved visualization, and
    generally improved code and docu

--- src/kyle/evaluation.py
+++ src/kyle/evaluation.py
@@ -1,8 +1,8 @@
 from typing import Union, Sequence
 
 import matplotlib.pyplot as plt
-import netcal.metrics
 import numpy as np
+from matplotlib.colors import ListedColormap
 
 from kyle.util import safe_accuracy_score
 
@@ -18,91 +18,235 @@ class EvalStats:
     :param bins: on how many homogeneous bins to evaluate the statistics
     """
 
-    def __init__(self, y_true: np.ndarray, confidences: np.ndarray, bins=20):
+    def __init__(self, y_true: np.ndarray, confidences: np.ndarray, bins=30):
         assert (
             len(y_true.shape) == 1
         ), f"y_true has to be 1-dimensional, instead got shape: {y_true.shape}"
         assert len(confidences.shape) == 2 and confidences.shape[0] == len(
             y_true
         ), f"predicted_probabilities have to be of shape (#samples, #classes), instead got {confidences.shape}"
-        self.n_samples = len(y_true)
+        self.num_samples = len(y_true)
+        self.num_classes = confidences.shape[1]
         self.y_true = y_true
         self.y_pred = confidences.argmax(axis=1)
         self.confidences = confidences
 
-        self.bins = None
-        self.confidence_bins = None
+        self.bins: int = None
+        # due to discretization they don't sum to 1 anymore
+        self.discretized_confidences: np.ndarray = None
+        self.discretized_probab_values: np.ndarray = None
         self.set_bins(bins)
+
+    def discretized_top_class_confidences(self) -> np.ndarray:
+        """
+        This is essentially the same as self.discretized_confidences.max(axis=1), up to ambiguities in maximum
+        due to collisions and discretization.
+        We avoid computing max and resolve collisions since we already have computed the argmax before.
+
+        :return: array of shape (N_samples, )
+        """
+        return np.choose(self.y_pred, self.discretized_confidences.T)
 
     def set_bins(self, bins: int):
         self.bins = bins
+        self.discretized_probab_values = (np.arange(self.bins) + 0.5) / self.bins
         bin_boundaries = np.linspace(0, 1, self.bins + 1)
         bin_boundaries[
             0
         ] = -1  # in order to associate predicted probabilities = 0 to the right bin
-        self.confidence_bins = (
+        binned_confidences = (
             np.digitize(x=self.confidences, bins=bin_boundaries, right=True) - 1
         )
+        binned_confidences = binned_confidences
+        self.discretized_confidences = (binned_confidences + 0.5) / self.bins
 
     def accuracy(self):
         return safe_accuracy_score(self.y_true, self.y_pred)
 
+    def expected_top_class_confidence(self):
+        return self.discretized_top_class_confidences().mean()
+
+    def marginal_accuracy(self, class_label: int):
+        """
+        Corresponds to acc_i in our calibration paper
+
+        :param class_label:
+        :return:
+        """
+        class_label_mask = self.y_pred == class_label
+        predictions = self.y_pred[class_label_mask]
+        gt = self.y_true[class_label_mask]
+        return np.sum(gt == predictions) / len(self.y_true)
+
+    def marginal_expected_top_class_confidence(self, class_label: int):
+        """
+        Corresponds to a term involved in ECE_i, given by the integral in formula (TODO)
+
+        :param class_label:
+        :return:
+        """
+        class_label_mask = self.y_pred == class_label
+        marginal_top_class_confidences = self.discretized_top_class_confidences()[
+            class_label_mask
+        ]
+        return marginal_top_class_confidences.sum() / len(self.y_true)
+
+    def _expected_error(
+        self, probabilities: np.ndarray, members_per_bin: np.ndarray
+    ) -> float:
+        """
+        Computes the expected error, being the sum of abs. differences of true probabilities and mean confidences
+        for each bin weighted by the factor N_bin / N_total
+
+        :param probabilities:
+        :param members_per_bin:
+        :return:
+        """
+        total_members = np.sum(members_per_bin)
+        if total_members == 0:
+            return 0.0
+        return (
+            np.sum(
+                np.abs(probabilities - self.discretized_probab_values) * members_per_bin
+            )
+            / total_members
+        )
+
+    def _non_degenerate_acc_conf_differences(self) -> np.ndarray:
+        """
+        Computes the absolute differences between accuracy and mean confidence for each non-degenerate bin
+        where a bin is considered degenerate if for no confidence vector the maximum lies in the bin.
+        E.g. for a N-classes classifier, all bins with right-hand value below 1/N will be degenerate since the
+        maximum of a probabilities vector is always larger than 1/N.
+
+        :return: array of shape (N_bins, )
+        """
+        accuracies, members_per_bin = self.top_class_reliabilities()
+        acc_conf_difference = (accuracies - self.discretized_probab_values)[
+            members_per_bin > 0
+        ]
+        return np.abs(acc_conf_difference)
+
     def expected_calibration_error(self):
-        return netcal.metrics.ECE(self.bins).measure(self.confidences, self.y_true)
+        accuracies, members_per_bin = self.top_class_reliabilities()
+        return self._expected_error(accuracies, members_per_bin)
 
     def average_calibration_error(self):
-        return netcal.metrics.ACE(self.bins).measure(self.confidences, self.y_true)
+        return np.mean(self._non_degenerate_acc_conf_differences())
 
     def max_calibration_error(self):
-        return netcal.metrics.MCE(self.bins).measure(self.confidences, self.y_true)
+        return np.max(self._non_degenerate_acc_conf_differences())
 
-    def marginal_reliability_hist(self, class_label: int):
-        class_confidence_bins = self.confidence_bins[:, class_label]
+    def expected_marginal_calibration_error(self, class_label):
+        """
+        I sort of made this up, although this very probably exists somewhere in the wild
+        :param class_label:
+        """
+        class_probabilities, members_per_bin = self.marginal_reliabilities(class_label)
+        return self._expected_error(class_probabilities, members_per_bin)
 
-        accuracies_per_bin = []
-        for confidence_bin in range(self.bins):
+    def average_marginal_calibration_error(self):
+        """
+        I made this up, don't know if this metric was described anywhere yet.
+        It is also not completely clear what this means in terms of probabilistic quantities.
+        """
+        errors = np.zeros(self.num_classes)
+        weights = np.zeros(self.num_classes)
+        for class_label in range(self.num_classes):
+            accuracies, n_members = self.marginal_reliabilities(class_label)
+            total_members = np.sum(n_members)
+            errors[class_label] = self._expected_error(accuracies, n_members)
+            weights[class_label] = total_members
+        return np.sum(errors * weights) / np.sum(weights)
+
+    def marginal_reliabilities(self, class_label: int):
+        """
+        Compute the true class probabilities and numbers of members (weights) for each of the N bins for the
+        confidence for the given class.
+
+        :return: tuple of two 1-dim arrays of length N, corresponding to (accuracy_per_bin, num_members_per_bin)
+        """
+        class_confidences = self.discretized_confidences[:, class_label]
+
+        members_per_bin = np.zeros(self.bins)
+        accuracies_per_bin = np.zeros(self.bins)
+        for i, probability_bin in enumerate(self.discretized_probab_values):
+            probability_bin_mask = class_confidences == probability_bin
             cur_gt_labels = self.y_true[
-                class_confidence_bins == confidence_bin
-            ]  # only consider gt in current bin
-            # cur_accuracy = np.count_nonzero(cur_gt_labels == class_label)/len(cur_gt_labels)
-            accuracies_per_bin.append(
-                safe_accuracy_score(
-                    cur_gt_labels, class_label * np.ones(len(cur_gt_labels))
-                )
+                probability_bin_mask
+            ]  # only consider gt in current probability bin
+
+            cur_members = np.sum(probability_bin_mask)
+            cur_accuracy = safe_accuracy_score(
+                cur_gt_labels, class_label * np.ones(len(cur_gt_labels))
             )
-        return np.arange(self.bins) / self.bins, np.array(accuracies_per_bin)
+            members_per_bin[i] = cur_members
+            accuracies_per_bin[i] = cur_accuracy
+        return accuracies_per_bin, members_per_bin
 
-    def top_class_reliability_hist(self):
-        top_class_confidence_bins = np.choose(self.y_pred, self.confidence_bins.T)
+    def top_class_reliabilities(self):
+        """
+        Compute the accuracies and numbers of members (weights) for each of the N bins for top-class confidence.
 
-        accuracies_per_bin = []
-        for confidence_bin in range(self.bins):
+        :return: tuple of two 1-dim arrays of length N, corresponding to (accuracy_per_bin, num_members_per_bin)
+        """
+        members_per_bin = np.zeros(self.bins)
+        accuracies_per_bin = np.zeros(self.bins)
+        for i, probability in enumerate(self.discretized_probab_values):
+            probability_bin_mask = (
+                self.discretized_top_class_confidences() == probability
+            )
             cur_gt_labels = self.y_true[
-                top_class_confidence_bins == confidence_bin
+                probability_bin_mask
             ]  # only consider gt in current bin
-            cur_pred_labels = self.y_pred[top_class_confidence_bins == confidence_bin]
-            accuracies_per_bin.append(
-                safe_accuracy_score(cur_gt_labels, cur_pred_labels)
-            )
-        return np.arange(self.bins) / self.bins, np.array(accuracies_per_bin)
+            cur_pred_labels = self.y_pred[probability_bin_mask]
 
-    def plot_reliability_curves(self, class_labels: Sequence[Union[int, str]]):
+            cur_members = np.sum(probability_bin_mask)
+            cur_accuracy = safe_accuracy_score(cur_gt_labels, cur_pred_labels)
+            members_per_bin[i] = cur_members
+            accuracies_per_bin[i] = cur_accuracy
+        return accuracies_per_bin, members_per_bin
+
+    def plot_reliability_curves(
+        self, class_labels: Sequence[Union[int, str]], display_weights=False
+    ):
+        """
+
+        :param class_labels:
+        :param display_weights: If True, for each reliability curve the weights of each bin will be
+            plotted as histogram. The weights have been scaled for the sake of display, only relative differences
+            between them have an interpretable meaning.
+            The errors containing "expected" in the name take these weights into account.
+        :return:
+        """
+        colors = ListedColormap(["y", "g", "r", "c", "m"])
+
         plt.figure()
         plt.title(f"Reliability curves")
         plt.xlabel("confidence")
         plt.ylabel("ground truth probability")
         plt.axis("equal")
-        plt.plot(np.linspace(0, 1), np.linspace(0, 1), label="perfect calibration")
-        for class_label in class_labels:
+        x_values = self.discretized_probab_values
+        plt.plot(
+            np.linspace(0, 1), np.linspace(0, 1), label="perfect calibration", color="b"
+        )
+        for i, class_label in enumerate(class_labels):
+            color = colors(i)
             if isinstance(class_label, int):
                 label = f"class {class_label}"
-                hist = self.marginal_reliability_hist(class_label)
+                y_values, weights = self.marginal_reliabilities(class_label)
             elif class_label == self.TOP_CLASS_LABEL:
                 label = "prediction"
-                hist = self.top_class_reliability_hist()
+                y_values, weights = self.top_class_reliabilities()
             else:
                 raise ValueError(f"Unknown class label: {class_label}")
-            plt.plot(*hist, marker="o", label=label)
+            plt.plot(x_values, y_values, marker=".", label=label, color=color)
+            if display_weights:
+                weights = weights / (
+                    3 * weights.max()
+                )  # rescale the weights such that the maximum is at 1/3 for improved visibility
+                plt.bar(x_values, weights, alpha=0.3, width=1 / self.bins, color=color)
+
         axes = plt.gca()
         axes.set_xlim([0, 1])
         axes.set_ylim([0, 1])
